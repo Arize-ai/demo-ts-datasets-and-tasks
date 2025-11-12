8fb4e173efa644774527a3fa2e89369d7eb424a3
Tony Powell <apowell@arize.com>
2025-11-12 16:18:41 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Phoenix Dataset Task Quickstart
+
+This is a quickstart using the Phoenix SDK to create a dataset and a task, run an experiment, and apply the task outputs into a new dataset for evaluation.
+
+It will produce a dataset with factual inputs and outputs, then it will invoke
+an llm to answer the inputs (without knowing the answers), and finally a new dataset will be created with the original inputs combined with the llm outputs.
+
+## Usage
+
+Start Phoenix locally (or use https://app.phoenix.arize.com):
+
+```bash
+docker start arize-phoenix 2>/dev/null || \
+  docker run --pull=always -d --name arize-phoenix --rm \
+  -p 6006:6006 \
+  # uncomment this if you do not want to persist phoenix data
+  # -e PHOENIX_SQL_DATABASE_URL="sqlite:///:memory:" \
+  arizephoenix/phoenix:latest
+```
+
+Configure environment variables:
+
+```bash
+cp .env.example .env
+# now edit .env
+```
+
+Install dependencies:
+
+```bash
+pnpm install
+```
+
+Run the quickstart:
+
+```bash
+# ensure you are running node 24
+# if you have nvm, run `nvm use`
+pnpm start
+```
---
+++ index.ts
@@ -0,0 +1,120 @@
+import { openai } from "@ai-sdk/openai";
+import { register } from "@arizeai/phoenix-otel";
+import {
+  createOrGetDataset,
+  getDataset,
+} from "@arizeai/phoenix-client/datasets";
+import { runExperiment } from "@arizeai/phoenix-client/experiments";
+import type { ExperimentTask } from "@arizeai/phoenix-client/types/experiments";
+import z from "zod";
+import { generateText } from "ai";
+
+const { PHOENIX_PROJECT_NAME, PHOENIX_HOST } = process.env;
+
+// Set up Phoenix tracing
+
+register({
+  projectName: PHOENIX_PROJECT_NAME,
+  // ensure traces are flushed immediately
+  batch: false,
+});
+
+// Create or get the dataset we will use for this quickstart
+
+const { datasetId } = await createOrGetDataset({
+  name: "phoenix-dataset-task-quickstart",
+  description: "A dataset for the phoenix dataset task quickstart",
+  examples: [
+    {
+      input: {
+        question: "What JS function is commonly used to make http requests?",
+      },
+      output: { answer: "fetch" },
+    },
+    {
+      input: {
+        question:
+          "What JS function is commonly used to apply transformation to each element of an array?",
+      },
+      output: { answer: "map" },
+    },
+    {
+      input: {
+        question:
+          "What JS data structure is commonly used to store a collection of key-value pairs?",
+      },
+      output: { answer: "object" },
+    },
+    {
+      input: {
+        question:
+          "What JS data structure is commonly used to ensure a value is only present once in a collection?",
+      },
+      output: { answer: "set" },
+    },
+  ],
+});
+const dataset = await getDataset({ dataset: { datasetId } });
+
+console.log(`📝 Reference dataset ${dataset.name} (${dataset.id}) created`);
+console.log(
+  `🔗 You can view this dataset at ${PHOENIX_HOST}/datasets/${dataset.id}/examples`
+);
+
+// Create a task that can answer questions about the dataset
+
+const task: ExperimentTask = async (example) => {
+  const { question } = z.object({ question: z.string() }).parse(example.input);
+  const { text } = await generateText({
+    model: openai("gpt-4o-mini"),
+    system: "You answer questions as tersely as possible.",
+    prompt: question,
+    experimental_telemetry: {
+      isEnabled: true,
+    },
+  });
+  return text;
+};
+
+// Create an experiment that will use the task to answer questions about the dataset
+
+const experiment = await runExperiment({
+  experimentName: "phoenix-experiment-task-quickstart",
+  experimentDescription: "An experiment for the phoenix task quickstart",
+  dataset: { datasetId },
+  task,
+});
+
+// Apply the task outputs into a new dataset that can be used for evaluation
+
+console.log(`🔄 Applying task outputs into a new dataset for evaluation...`);
+
+const timestamp = new Date().toISOString();
+const { datasetId: evaluationDatasetId } = await createOrGetDataset({
+  name: `phoenix-evaluation-dataset-task-quickstart-${timestamp}`,
+  description: "A dataset for the phoenix task quickstart evaluation",
+  examples: Object.values(experiment.runs).flatMap((run) => {
+    const originalExample = dataset.examples.find(
+      (example) => example.id === run.datasetExampleId
+    );
+    if (!originalExample) {
+      return [];
+    }
+    return [
+      {
+        input: { ...originalExample.input, ...originalExample.output },
+        output: { answer: run.output },
+      },
+    ];
+  }),
+});
+
+const evaluationDataset = await getDataset({
+  dataset: { datasetId: evaluationDatasetId },
+});
+console.log(
+  `📝 Evaluation dataset ${evaluationDataset.name} (${evaluationDataset.id}) created`
+);
+console.log(
+  `🔗 You can view this dataset at ${PHOENIX_HOST}/datasets/${evaluationDataset.id}/examples`
+);
